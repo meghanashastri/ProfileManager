5342c2ae75d6ba90ade04405fb64fcd203e763be
Meghana <meghanashastri07@gmail.com>
2016-03-27 22:21:33 +0530

    initial commit

---
+++ app/src/main/java/com/example/admin/profilemanager/activities/SplashScreen.java
@@ -0,0 +1,9 @@
+package com.example.admin.profilemanager.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Admin on 27-03-2016.
+ */
+public class SplashScreen extends AppCompatActivity {
+}
