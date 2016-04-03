ae04279e618602596cdd7c6eeb521c0b64cfafef
Meghana <meghanashastri07@gmail.com>
2016-04-03 15:53:02 +0530

    click listeners added in main activity

--- app/src/main/java/com/example/admin/profilemanager/activities/MainActivity.java
+++ app/src/main/java/com/example/admin/profilemanager/activities/MainActivity.java
@@ -1,17 +1,57 @@
 package com.example.admin.profilemanager.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.example.admin.profilemanager.R;
 
 /**
  * Created by Admin on 27-03-2016.
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private RelativeLayout tvMovementOfUser,tvTimer,tvBatteryOfPhone,tvLocationOfUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialiseViews();
     }
+    public void initialiseViews(){
+        tvLocationOfUser=(RelativeLayout) findViewById(R.id.location);
+        tvLocationOfUser.setOnClickListener(this);
+        tvMovementOfUser=(RelativeLayout) findViewById(R.id.movement);
+        tvMovementOfUser.setOnClickListener(this);
+        tvTimer=(RelativeLayout) findViewById(R.id.timer);
+        tvTimer.setOnClickListener(this);
+        tvBatteryOfPhone=(RelativeLayout) findViewById(R.id.battery);
+        tvBatteryOfPhone.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.timer:
+                goToNextActivity(TimerActivity.class);
+                break;
+            case R.id.movement:
+               goToNextActivity(MovementActivity.class);
+                break;
+            case R.id.battery:
+                goToNextActivity(BatteryActivity.class);
+                break;
+            case R.id.location:
+                goToNextActivity(LocationActivity.class);
+                break;
+        }
+
+    }
+
+    protected void goToNextActivity(Class nextActivity) {
+        Intent intent = new Intent();
+        intent.setClass(this, nextActivity);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/admin/profilemanager/activities/TimerActivity.java
@@ -0,0 +1,9 @@
+package com.example.admin.profilemanager.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Admin on 03-04-2016.
+ */
+public class TimerActivity extends AppCompatActivity {
+}
